e9ec9ff170a9ca6777259b2fb59c21fc6954d977
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2017-05-02 11:00:09 +0000

    fix layout of tweets/index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,26 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  respond_to :js
 
   def create
     @user_id = current_user.id
-    @tweet_id = Tweet.find(params[:id]).id
+    @tweet = Tweet.find(params[:id])
+    @tweet_id = @tweet.id
     @favorite = Favorite.new(tweet_id: @tweet_id, user_id: @user_id )
 
     if @favorite.save
-      redirect_to tweets_path
+      respond_with @tweet
     end
   end
 
   def destroy
     @user_id = current_user.id
-    @tweet_id = Tweet.find(params[:id]).id
+    @tweet = Tweet.find(params[:id])
+    @tweet_id = @tweet.id
     @favorite = Favorite.find_by(tweet_id: @tweet_id, user_id: @user_id )
 
     if @favorite.destroy
-      redirect_to tweets_path
+      respond_with @tweet
     end
   end
 end
